8669286dd871db6cb7532b9c600e30dc289cfa29
Mahmoud Gabr <mhmudhsham8@gmail.com>
2023-05-16 00:17:54 +0400

    Admin Service CRUD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            SliderPermissionSeeder::class
+            SliderPermissionSeeder::class,
+            ServicePermissionSeeder::class,
         ]);
     }
 }
